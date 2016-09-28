d2ad98b6dad2e226afae958afc0422964edd07e5
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-09-28 11:27:37 +0900

    change Waiting

--- participant/Waiting.js
+++ participant/Waiting.js
@@ -12,6 +12,8 @@ const Waiting = ({ joined }) => (
 	<Card>
 		<CardTitle title="出席システム" subtitle="待機画面" />
 		<CardText>
+			<p>参加者の登録を待っています。</p>
+			<p>この画面のまましばらくお待ち下さい。</p>
 			<p>現在{joined}人が参加しています。 </p>
 		</CardText>
 		<div style={{textAlign: "center"}}>
